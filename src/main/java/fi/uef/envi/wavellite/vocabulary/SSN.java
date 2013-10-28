83d669d64f9c0742ec983255a66101d9a970d0b5
Markus Stocker <markus.stocker@gmail.com>
2013-10-28 19:11:49 +0200

    First commit

---
+++ src/main/java/fi/uef/envi/wavellite/vocabulary/SSN.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (C) 2011 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.vocabulary;
+
+/**
+ * <p>
+ * Title: SSN
+ * </p>
+ * <p>
+ * Description: Semantic Sensor Network vocabulary
+ * </p>
+ * <p>
+ * Project: Wavellite Core
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2011
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class SSN {
+
+	/** http://purl.oclc.org/NET/ssnx/ssn */
+	public static final String ns = "http://purl.oclc.org/NET/ssnx/ssn";
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#Sensor */
+	public static final String Sensor = _("Sensor");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#SensingDevice */
+	public static final String SensingDevice = _("SensingDevice");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#Observation */
+	public static final String Observation = _("Observation");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#FeatureOfInterest */
+	public static final String FeatureOfInterest = _("FeatureOfInterest");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#Property */
+	public static final String Property = _("Property");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#SensorOutput */
+	public static final String SensorOutput = _("SensorOutput");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#ObservationValue */
+	public static final String ObservationValue = _("ObservationValue");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#System */
+	public static final String System = _("System");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#featureOfInterest */
+	public static final String featureOfInterest = _("featureOfInterest");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#observedProperty */
+	public static final String observedProperty = _("observedProperty");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#observedBy */
+	public static final String observedBy = _("observedBy");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#hasProperty */
+	public static final String hasProperty = _("hasProperty");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#hasValue */
+	public static final String hasValue = _("hasValue");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#observationResultTime */
+	public static final String observationResultTime = _("observationResultTime");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#observationResult */
+	public static final String observationResult = _("observationResult");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#isPropertyOf */
+	public static final String isPropertyOf = _("isPropertyOf");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#observes */
+	public static final String observes = _("observes");
+
+	/** http://purl.oclc.org/NET/ssnx/ssn#qualityOfObservation */
+	public static final String qualityOfObservation = _("qualityOfObservation");
+
+	private static String _(String fragment) {
+		return ns + "#" + fragment;
+	}
+	
+}
---
+++ src/main/java/fi/uef/envi/wavellite/vocabulary/WO.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2013 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.vocabulary;
+
+/**
+ * <p>
+ * Title: WO
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Vocabulary
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2013
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class WO {
+
+	/** http://envi.uef.fi/wavellite */
+	public static final String ns = "http://envi.uef.fi/wavellite";
+	
+	/** http://envi.uef.fi/wavellite#Entity */
+	public static final String Entity = _("Entity");
+	
+	/** http://envi.uef.fi/wavellite#MeasurementValue */
+	public static final String MeasurementValue = _("MeasurementValue");
+	
+	/** http://envi.uef.fi/wavellite#DoubleMeasurementValue */
+	public static final String DoubleMeasurementValue = _("DoubleMeasurementValue");
+	
+	/** http://envi.uef.fi/wavellite#Sensor */
+	public static final String Sensor = _("Sensor");
+	
+	/** http://envi.uef.fi/wavellite#Feature */
+	public static final String Feature = _("Feature");
+	
+	/** http://envi.uef.fi/wavellite#Property */
+	public static final String Property = _("Property");
+	
+	private static String _(String fragment) {
+		return ns + "#" + fragment;
+	}
+	
+}
