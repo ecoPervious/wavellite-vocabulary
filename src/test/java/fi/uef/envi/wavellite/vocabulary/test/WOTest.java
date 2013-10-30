2e717bf914fa5d294c82893fc0bd8aba071628b8
Markus Stocker <markus.stocker@gmail.com>
2013-10-30 10:48:03 +0200

    + Added src/test/java with empty test for Wavellite Ontology

---
+++ src/test/java/fi/uef/envi/wavellite/vocabulary/test/WOTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2013 see CREDITS.txt
+ * All rights reserved.
+ */
+
+package fi.uef.envi.wavellite.vocabulary.test;
+
+/**
+ * <p>
+ * Title: WOTest
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Project: Wavellite Vocabulary
+ * </p>
+ * <p>
+ * Copyright: Copyright (C) 2013
+ * </p>
+ * 
+ * @author Markus Stocker
+ */
+
+public class WOTest {
+
+}
